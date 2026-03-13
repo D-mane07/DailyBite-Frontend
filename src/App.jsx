07ca55b23575636c85932966ce4380fbb7cc3335
Dhanyakumar Mane <dhanyakumardm23hecse@student.mes.ac.in>
2026-03-13 17:51:25 +0000

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ import TrackorderPage from './components/TrackorderPage.jsx'
 import Shop from './components/Shop.jsx'
 import RouteMetadata from './components/RouteMetadata.jsx'
 
-export const serverurl="https://20.193.151.157:8000"
+export const serverurl="http://20.193.151.157:8000"
 const APP = () => {
    const{userData}=useSelector(state=>state.user)
   const authChecked = UseGetCurrentUser();
