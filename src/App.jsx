6d6118406dd755f86c5b77d569d7e35c1352bed0
Dhanyakumar Mane <dhanyakumardm23hecse@student.mes.ac.in>
2026-03-15 09:51:43 +0000

    fix backend URL to permanent domain

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ import TrackorderPage from './components/TrackorderPage.jsx'
 import Shop from './components/Shop.jsx'
 import RouteMetadata from './components/RouteMetadata.jsx'
 
-export const serverurl="https://assumes-segments-trans-authorization.trycloudflare.com"
+export const serverurl="https://dailybite.ddns.net"
 const APP = () => {
    const{userData}=useSelector(state=>state.user)
   const authChecked = UseGetCurrentUser();
